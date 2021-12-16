6b38f3c1a6e650393c04b5ceea76e67b9f9938cc
Ankit Srivastava <ankitsonu5@gmail.com>
2021-12-16 14:17:20 +0530

    add photogallery
    photogallery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
 (function() {
     window.onload = function() { window.setTimeout(fadeout, 500); }
 
-    function fadeout() { document.querySelector('.preloader').style.opacity = '0';
-        document.querySelector('.preloader').style.display = 'none'; }
+    function fadeout() {
+        document.querySelector('.preloader').style.opacity = '0';
+        document.querySelector('.preloader').style.display = 'none';
+    }
     window.onscroll = function() { var header_navbar = document.querySelector(".navbar-area"); var sticky = header_navbar.offsetTop; var backToTo = document.querySelector(".scroll-top"); if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) { backToTo.style.display = "flex"; } else { backToTo.style.display = "none"; } };
     let navbarToggler = document.querySelector(".mobile-menu-btn");
     navbarToggler.addEventListener('click', function() { navbarToggler.classList.toggle("active"); });
 })();
+
+document.addEventListener("click", function(e) {
+    if (e.target.classList.contains("gallery-item")) {
+        const src = e.target.getAttribute("src");
+        document.querySelector(".modal-img").src = src;
+        const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+        myModal.show();
+    }
+})
